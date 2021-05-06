4cb79e4168d61df9bfc22203c822a35421fb5cf4
Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>
2021-05-06 09:39:17 +0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '255a8bb6c62d421ce19160f15bea8aa9'),
+    },
+  },
+});
