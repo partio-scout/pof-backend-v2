c6710204d3f9250d191fab51567f1e9ccec70d3f
Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>
2021-06-08 12:09:08 +0300

    Imrpoved migration's axios error reporting

--- migrations/utils/axios.js
+++ migrations/utils/axios.js
@@ -1,13 +1,15 @@
 const axios = require("axios");
-const rateLimit = require('axios-rate-limit');
-const { readFileSync, existsSync, exists } = require('fs');
+const rateLimit = require("axios-rate-limit");
+const { readFileSync, existsSync, exists } = require("fs");
 
 const createInstance = () => {
   // Read the config from file
-  const configPath = 'migrations/data/config.json';
+  const configPath = "migrations/data/config.json";
 
   if (!existsSync(configPath)) {
-    throw new Error('No config file present. Create it to `migrations/data/config.json`');
+    throw new Error(
+      "No config file present. Create it to `migrations/data/config.json`"
+    );
   }
 
   const configJson = readFileSync(configPath);
@@ -19,9 +21,28 @@ const createInstance = () => {
       Authorization: `Bearer ${config.strapiAdminToken}`,
     },
   });
-  return axiosInstance;
-}
 
+  axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response) {
+        throw new Error(
+          JSON.stringify({
+            status: error.response?.status,
+            statusText: error.response?.statustext,
+            url: error.response?.config.baseURL + error.response?.config.url,
+            method: error.response?.config.method,
+            inputData: error.response?.config.data,
+            outputMessage: error.response?.data.message,
+            outputData: error.response?.data.data,
+          }, undefined, 4)
+        );
+      }
+      throw error;
+    }
+  );
+  return axiosInstance;
+};
 
 const rateLimitedInstance = rateLimit(createInstance(), { maxRPS: 5 });
 
