fa9587b0b48532c00a9282f4ab17446bfe1178e4
Tuomas Inkinen <tuomasink@gmail.com>
2021-06-23 14:42:45 +0300

    POF-28: Suggestion likes (#7)
    * Created endpoint to suggestion content type for creating new suggestions from the website
    
    * Added liking functionality to Suggestions
    
    * Errors for like mishaps
    
    Co-authored-by: Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>

--- api/suggestion/controllers/suggestion.js
+++ api/suggestion/controllers/suggestion.js
@@ -8,6 +8,83 @@ const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
   /**
+   * Endpoint for creating new suggestions. This saves the suggestions in draft state,
+   * so that they can be later published by admins.
+   */
+  async new(ctx) {
+    let entity, data, files;
+
+    if (ctx.is("multipart")) {
+      ({ data, files } = parseMultipartData(ctx));
+    } else {
+      data = ctx.request.body;
+    }
+
+    // Set published_at null so the entry will be created in draft state
+    data.published_at = null;
+    data.from_web = true;
+
+    entity = await strapi.services.suggestion.create(
+      data,
+      files ? { files } : undefined
+    );
+
+    return sanitizeEntity(entity, { model: strapi.models.suggestion });
+  },
+  /**
+   * Endpoint for liking suggestions.
+   */
+  async like(ctx) {
+    const { id } = ctx.params;
+    const { user } = ctx.request.body;
+
+    let entity = await strapi.services.suggestion.findOne({ id });
+
+    if (!entity) {
+      ctx.response.status = 404;
+      return;
+    }
+
+    if (entity.likes?.includes(user)) {
+      ctx.response.status = 400;
+      ctx.response.body = 'already liked';
+      return;
+    }
+
+    entity.likes = [...(entity.likes || []), user];
+
+    entity = await strapi.services.suggestion.update({ id }, entity);
+
+    return sanitizeEntity(entity, { model: strapi.models.suggestion });
+  },
+  /**
+   * Endpoint for unliking suggestions.
+   */
+  async unlike(ctx) {
+    const { id } = ctx.params;
+    const { user } = ctx.request.body;
+
+    let entity = await strapi.services.suggestion.findOne({ id });
+
+    if (!entity) {
+      ctx.response.status = 404;
+      return;
+    }
+
+    const index = entity.likes?.findIndex((x) => x === user);
+
+    if (index < 0) {
+      ctx.response.status = 400;
+      ctx.response.body = 'not liked';
+      return;
+    }
+
+    entity.likes.splice(index, 1);
+    entity = await strapi.services.suggestion.update({ id }, entity);
+
+    return sanitizeEntity(entity, { model: strapi.models.suggestion });
+  },
+  /**
    * Endpoint for creating new comments. This saves the comments in draft state,
    * so that they can be later published by admins.
    */
@@ -17,9 +94,9 @@ module.exports = {
     let data;
 
     // Check that the suggestion exists
-    const suggestions = await strapi.services.suggestion.find({ id });
+    const suggestion = await strapi.services.suggestion.findOne({ id });
 
-    if (suggestions.length === 0) {
+    if (!suggestion) {
       ctx.response.status = 404;
       return;
     }
@@ -37,29 +114,5 @@ module.exports = {
     await strapi.services.comment.create(data);
 
     ctx.response.status = 200;
-  },
-  /*
-   * Endpoint for creating new suggestions. This saves the suggestions in draft state,
-   * so that they can be later published by admins.
-   */
-  async new(ctx) {
-    let data, files;
-
-    if (ctx.is("multipart")) {
-      ({ data, files } = parseMultipartData(ctx));
-    } else {
-      data = ctx.request.body;
-    }
-
-    // Set published_at null so the entry will be created in draft state
-    data.published_at = null;
-    data.from_web = true;
-
-    const entity = await strapi.services.suggestion.create(
-      data,
-      files ? { files } : undefined
-    );
-
-    return sanitizeEntity(entity, { model: strapi.models.suggestion });
   },
 };
--- api/suggestion/models/suggestion.js
+++ api/suggestion/models/suggestion.js
@@ -5,9 +5,20 @@ const { createLifecycleHooks } = require("../../../utils/algolia");
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
-
 const contentType = "suggestion";
 
 module.exports = {
-  lifecycles: createLifecycleHooks(contentType),
+  lifecycles: {
+    async beforeUpdate(params, data) {
+      // Set the suggestion's like_count based on the current likes
+      data.like_count = data.likes?.length || 0;
+    },
+    async afterCreate(result, data) {
+      if (result.from_web) {
+        // TODO: Send email to interested parties
+        // console.log('Sending notification about new suggestion from web');
+      }
+    },
+    ...createLifecycleHooks(contentType),
+  },
 };
