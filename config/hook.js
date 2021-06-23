a00045d5768ef08dd635ddb4a40c70142d66d799
Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>
2021-06-23 11:48:06 +0300

    Removed commented code

--- config/hook.js
+++ config/hook.js
@@ -5,7 +5,6 @@ module.exports =({ env }) => ({
       applicationId: env('ALGOLIA_APPLICATION_ID'),
       apiKey: env('ALGOLIA_API_KEY'),
       debug: true,              // default: false
-      // prefix: 'my_own_prefix'   // default: Strapi environment (strapi.config.environment)
     },
   }
 });
