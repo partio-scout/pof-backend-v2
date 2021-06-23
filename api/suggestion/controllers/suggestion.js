e59fc43961baaae77c6307be9eff2b7a959becf9
Tuomas Inkinen <tuomasink@gmail.com>
2021-06-23 13:29:15 +0300

    POF-29: Suggestion comments (#8)
    * Created endpoint /api/suggestions/{id}/comment for creating comments linked to a suggestion
    
    * Check that suggestion exists when commenting it
    
    Co-authored-by: Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>

--- api/suggestion/controllers/suggestion.js
+++ api/suggestion/controllers/suggestion.js
@@ -8,6 +8,37 @@ const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
   /**
+   * Endpoint for creating new comments. This saves the comments in draft state,
+   * so that they can be later published by admins.
+   */
+  async comment(ctx) {
+    const { id } = ctx.params;
+
+    let data;
+
+    // Check that the suggestion exists
+    const suggestions = await strapi.services.suggestion.find({ id });
+
+    if (suggestions.length === 0) {
+      ctx.response.status = 404;
+      return;
+    }
+
+    if (ctx.is("multipart")) {
+      ({ data } = parseMultipartData(ctx));
+    } else {
+      data = ctx.request.body;
+    }
+
+    // Set published_at null so the entry will be created in draft state
+    data.published_at = null;
+    data.suggestion = id;
+
+    await strapi.services.comment.create(data);
+
+    ctx.response.status = 200;
+  },
+  /*
    * Endpoint for creating new suggestions. This saves the suggestions in draft state,
    * so that they can be later published by admins.
    */
