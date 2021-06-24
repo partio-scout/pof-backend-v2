aedc4575e17bce7b491af10ef6b4899be051327c
Tuomas Inkinen <tuomasink@gmail.com>
2021-06-24 11:18:28 +0300

    POF-48: New suggestion notifications (#10)
    * Created endpoint to suggestion content type for creating new suggestions from the website
    
    * Added sendgrid plugin
    
    * Created Settings content type
    
    * Created email notification about new suggestions from web
    
    * Update
    
    * Trim whitespace from email addresses
    
    * Handle email errors
    
    * Added more time for tests
    
    Co-authored-by: Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>

--- api/suggestion/models/suggestion.js
+++ api/suggestion/models/suggestion.js
@@ -1,23 +1,53 @@
 "use strict";
 const { createLifecycleHooks } = require("../../../utils/algolia");
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
- * to customize this model
- */
 const contentType = "suggestion";
+
+const notifyAboutSuggestion = async (suggestion) => {
+  const settings = await getSettings();
+
+  const recipients = settings.suggestion_notification_recipients
+    ?.split(",")
+    .map((email) => email.trim());
+
+  await Promise.all(
+    recipients.map(
+      async (recipient) =>
+        await sendSuggestionNotification(recipient, suggestion)
+    )
+  );
+};
+
+const getSettings = async () => {
+  return await strapi.services.settings.find();
+};
+
+const sendSuggestionNotification = async (recipient, suggestion) => {
+  console.log("Sending new suggestion notification to:", recipient);
+
+  const emailConfig = {
+    to: recipient,
+    subject: "Uusi toteutusehdotus",
+    text: `Partio-ohjelmaan on lähetetty uusi toteutusehdotus: ${suggestion.title}, kirjoittaja: ${suggestion.author}.`,
+  };
+
+  try {
+    await strapi.plugins.email.services.email.send(emailConfig);
+  } catch (error) {
+    console.error("Error while sending email:", error);
+  }
+};
 
 module.exports = {
   lifecycles: {
+    async afterCreate(result, data) {
+      if (result.from_web) {
+        await notifyAboutSuggestion(result);
+      }
+    },
     async beforeUpdate(params, data) {
       // Set the suggestion's like_count based on the current likes
       data.like_count = data.likes?.length || 0;
-    },
-    async afterCreate(result, data) {
-      if (result.from_web) {
-        // TODO: Send email to interested parties
-        // console.log('Sending notification about new suggestion from web');
-      }
     },
     ...createLifecycleHooks(contentType),
   },
