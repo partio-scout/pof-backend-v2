1c12a583d75a220c8e2415d73ef63be15f50e0f0
Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>
2021-06-24 13:44:58 +0300

    Updated tests for suggestion controller

--- tests/suggestion/index.js
+++ tests/suggestion/index.js
@@ -1,188 +1,217 @@
 const request = require("supertest");
-const { describe } = require("yargs");
 const { grantPriviledge } = require("../helpers/strapi");
 
 describe("Suggestions controller", () => {
   beforeAll(async () => {
     // Grant priviledges to to controllers so they can be invoked anonymously
-    await grantPriviledge(
-      2,
-      "permissions.application.controllers.suggestion.new"
-    );
-    await grantPriviledge(
-      2,
-      "permissions.application.controllers.suggestion.like"
-    );
-    await grantPriviledge(
-      2,
-      "permissions.application.controllers.suggestion.unlike"
-    );
-    await grantPriviledge(
-      2,
-      "permissions.application.controllers.suggestion.comment"
-    );
+    await Promise.all([
+      await grantPriviledge(
+        2,
+        "permissions.application.controllers.suggestion.new"
+      ),
+      await grantPriviledge(
+        2,
+        "permissions.application.controllers.suggestion.like"
+      ),
+      await grantPriviledge(
+        2,
+        "permissions.application.controllers.suggestion.unlike"
+      ),
+      await grantPriviledge(
+        2,
+        "permissions.application.controllers.suggestion.comment"
+      ),
+    ]);
   });
 
-  it("`new` should create new suggestion with `from_web` true and `published_at` null", async () => {
-    await request(strapi.server)
-      .post("/suggestions/new")
-      .set("Content-Type", "application/json")
-      .send({
+  describe("/suggestions/new", () => {
+    it("should create new suggestion with `from_web` true and `published_at` null", async () => {
+      await request(strapi.server)
+        .post("/suggestions/new")
+        .set("Content-Type", "application/json")
+        .send({
+          title: "title",
+          content: "content",
+          author: "author",
+        })
+        .expect(200)
+        .then((data) => {
+          expect(data.body.title).toEqual("title");
+          expect(data.body.content).toEqual("content");
+          expect(data.body.author).toEqual("author");
+          expect(data.body.published_at).toBeNull();
+          expect(data.body.from_web).toEqual(true);
+        });
+    });
+  });
+
+  describe("/suggestions/<id>/like", () => {
+    it("should increase like_count", async () => {
+      // create a suggestion
+      const suggestion = await strapi.services.suggestion.create({
         title: "title",
         content: "content",
         author: "author",
-      })
-      .expect(200)
-      .then((data) => {
-        expect(data.body.title).toEqual("title");
-        expect(data.body.content).toEqual("content");
-        expect(data.body.author).toEqual("author");
-        expect(data.body.published_at).toBeNull();
-        expect(data.body.from_web).toEqual(true);
       });
+
+      await request(strapi.server)
+        .post(`/suggestions/${suggestion.id}/like`)
+        .set("Content-Type", "application/json")
+        .send({
+          user: "user1",
+        })
+        .expect(200)
+        .then((data) => {
+          expect(data.body.id).toEqual(suggestion.id);
+          expect(data.body.like_count).toEqual(1);
+        });
+    });
+    it("should not allow multiple likes to same suggestion from one user", async () => {
+      // create a suggestion
+      const suggestion = await strapi.services.suggestion.create({
+        title: "title",
+        content: "content",
+        author: "author",
+      });
+
+      await request(strapi.server)
+        .post(`/suggestions/${suggestion.id}/like`)
+        .set("Content-Type", "application/json")
+        .send({
+          user: "user1",
+        })
+        .expect(200)
+        .then((data) => {
+          expect(data.body.id).toEqual(suggestion.id);
+          expect(data.body.like_count).toEqual(1);
+        });
+
+      await request(strapi.server)
+        .post(`/suggestions/${suggestion.id}/like`)
+        .set("Content-Type", "application/json")
+        .send({
+          user: "user1",
+        })
+        .expect(400)
+        .then((data) => {
+          expect(data.text).toEqual("already liked");
+        });
+    });
+    it("should return 404 when liking nonexistent suggestion", async () => {
+      // Create a comment to that suggesion
+      await request(strapi.server)
+        .post(`/suggestions/99/like`)
+        .set("Content-Type", "application/json")
+        .send({
+          user: "test",
+        })
+        .expect(404);
+    });
   });
 
-  it("`like` should increase like_count", async () => {
-    // create a suggestion
-    const suggestion = await strapi.services.suggestion.create({
-      title: "title",
-      content: "content",
-      author: "author",
+  describe("/suggestions/<id>/unlike", () => {
+    it("should decrease like_count", async () => {
+      // create a suggestion
+      const suggestion = await strapi.services.suggestion.create({
+        title: "title",
+        content: "content",
+        author: "author",
+      });
+
+      await request(strapi.server)
+        .post(`/suggestions/${suggestion.id}/like`)
+        .set("Content-Type", "application/json")
+        .send({
+          user: "user1",
+        })
+        .expect(200)
+        .then((data) => {
+          expect(data.body.id).toEqual(suggestion.id);
+          expect(data.body.like_count).toEqual(1);
+        });
+
+      await request(strapi.server)
+        .post(`/suggestions/${suggestion.id}/unlike`)
+        .set("Content-Type", "application/json")
+        .send({
+          user: "user1",
+        })
+        .expect(200)
+        .then((data) => {
+          expect(data.body.id).toEqual(suggestion.id);
+          expect(data.body.like_count).toEqual(0);
+        });
     });
 
-    await request(strapi.server)
-      .post(`/suggestions/${suggestion.id}/like`)
-      .set("Content-Type", "application/json")
-      .send({
-        user: "user1",
-      })
-      .expect(200)
-      .then((data) => {
-        expect(data.body.id).toEqual(suggestion.id);
-        expect(data.body.like_count).toEqual(1);
+    it("should not allow unliking of a not liked suggestion", async () => {
+      // create a suggestion
+      const suggestion = await strapi.services.suggestion.create({
+        title: "title",
+        content: "content",
+        author: "author",
       });
-  });
-  it("`like` should not allow multiple likes to same suggestion from one user", async () => {
-    // create a suggestion
-    const suggestion = await strapi.services.suggestion.create({
-      title: "title",
-      content: "content",
-      author: "author",
+
+      await request(strapi.server)
+        .post(`/suggestions/${suggestion.id}/unlike`)
+        .set("Content-Type", "application/json")
+        .send({
+          user: "user1",
+        })
+        .expect(400)
+        .then((data) => {
+          expect(data.text).toEqual("not liked");
+        });
     });
-
-    await request(strapi.server)
-      .post(`/suggestions/${suggestion.id}/like`)
-      .set("Content-Type", "application/json")
-      .send({
-        user: "user1",
-      })
-      .expect(200)
-      .then((data) => {
-        expect(data.body.id).toEqual(suggestion.id);
-        expect(data.body.like_count).toEqual(1);
-      });
-
-    await request(strapi.server)
-      .post(`/suggestions/${suggestion.id}/like`)
-      .set("Content-Type", "application/json")
-      .send({
-        user: "user1",
-      })
-      .expect(400)
-      .then((data) => {
-        expect(data.text).toEqual("already liked");
-      });
-  });
-
-  it("`unlike` should decrease like_count", async () => {
-    // create a suggestion
-    const suggestion = await strapi.services.suggestion.create({
-      title: "title",
-      content: "content",
-      author: "author",
+    it("should return 404 when unliking nonexistent suggestion", async () => {
+      // Create a comment to that suggesion
+      await request(strapi.server)
+        .post(`/suggestions/99/unlike`)
+        .set("Content-Type", "application/json")
+        .send({
+          user: "test",
+        })
+        .expect(404);
     });
-
-    await request(strapi.server)
-      .post(`/suggestions/${suggestion.id}/like`)
-      .set("Content-Type", "application/json")
-      .send({
-        user: "user1",
-      })
-      .expect(200)
-      .then((data) => {
-        expect(data.body.id).toEqual(suggestion.id);
-        expect(data.body.like_count).toEqual(1);
-      });
-
-    await request(strapi.server)
-      .post(`/suggestions/${suggestion.id}/unlike`)
-      .set("Content-Type", "application/json")
-      .send({
-        user: "user1",
-      })
-      .expect(200)
-      .then((data) => {
-        expect(data.body.id).toEqual(suggestion.id);
-        expect(data.body.like_count).toEqual(0);
-      });
   });
 
-  it("`unlike` should not allow unliking of a not liked suggestion", async () => {
-    // create a suggestion
-    const suggestion = await strapi.services.suggestion.create({
-      title: "title",
-      content: "content",
-      author: "author",
-    });
-
-    await request(strapi.server)
-      .post(`/suggestions/${suggestion.id}/unlike`)
-      .set("Content-Type", "application/json")
-      .send({
-        user: "user1",
-      })
-      .expect(400)
-      .then((data) => {
-        expect(data.text).toEqual("not liked");
+  describe("/suggestions/<id>/comment", () => {
+    it("should create a draft comment and link it to the suggestion", async () => {
+      // Create a suggestion
+      const suggestion = await strapi.services.suggestion.create({
+        title: "title",
+        content: "content",
+        author: "author",
       });
-  });
 
-  it("`comment` should create a draft comment and link it to the suggestion", async () => {
-    // Create a suggestion
-    const suggestion = await strapi.services.suggestion.create({
-      title: "title",
-      content: "content",
-      author: "author",
+      // Create a comment to that suggesion
+      await request(strapi.server)
+        .post(`/suggestions/${suggestion.id}/comment`)
+        .set("Content-Type", "application/json")
+        .send({
+          text: "A test comment",
+          author: "tester",
+        })
+        .expect(200);
+
+      // Check that the comment is linked to the suggestion and it is not published.
+      // By using `starpi.query()` we get also unpublished content.
+      const updatedSuggestion = await strapi.query("suggestion").findOne({
+        id: suggestion.id,
+      });
+      expect(updatedSuggestion.comments.length).toEqual(1);
+      expect(updatedSuggestion.comments[0].text).toEqual("A test comment");
+      expect(updatedSuggestion.comments[0].published_at).toBeNull();
     });
-
-    // Create a comment to that suggesion
-    await request(strapi.server)
-      .post(`/suggestions/${suggestion.id}/comment`)
-      .set("Content-Type", "application/json")
-      .send({
-        text: "A test comment",
-        author: "tester",
-      })
-      .expect(200);
-
-    // Check that the comment is linked to the suggestion and it is not published.
-    // By using `starpi.query()` we get also unpublished content.
-    const updatedSuggestion = await strapi.query("suggestion").findOne({
-      id: suggestion.id,
+    it("should return 404 when commenting nonexistent suggestion", async () => {
+      // Create a comment to that suggesion
+      await request(strapi.server)
+        .post(`/suggestions/99/comment`)
+        .set("Content-Type", "application/json")
+        .send({
+          text: "A test comment",
+          author: "tester",
+        })
+        .expect(404);
     });
-    expect(updatedSuggestion.comments.length).toEqual(1);
-    expect(updatedSuggestion.comments[0].text).toEqual("A test comment");
-    expect(updatedSuggestion.comments[0].published_at).toBeNull();
-  });
-  it("`comment` should return 404 when commenting nonexistent suggestion", async () => {
-    // Create a comment to that suggesion
-    await request(strapi.server)
-      .post(`/suggestions/99/comment`)
-      .set("Content-Type", "application/json")
-      .send({
-        text: "A test comment",
-        author: "tester",
-      })
-      .expect(404);
   });
 });
