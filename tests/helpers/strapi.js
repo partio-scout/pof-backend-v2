02f62d64a071ac0ab80da7f203aff76a4048f0d2
Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>
2021-06-28 15:53:25 +0300

    Updated grantPriviledge's docs

--- tests/helpers/strapi.js
+++ tests/helpers/strapi.js
@@ -5,10 +5,10 @@ let instance;
 
 async function setupStrapi() {
   if (!instance) {
-    /** 
+    /**
      * The following code in copied from `./node_modules/strapi/lib/Strapi.js`.
-     * 
-     * Strapi() call sets strapi as a global variable. 
+     *
+     * Strapi() call sets strapi as a global variable.
      * */
 
     instance = await Strapi().load();
@@ -22,19 +22,18 @@ async function setupStrapi() {
 }
 
 /**
- * Grants database `permissions` table that role can access an endpoint/controllers
+ * Grants priviledge for a role to access a controller's route. For example with roleID: `2` and value:
+ * "permissions.application.controllers.suggestion.new", you allow `Public` role to access `suggestion.new` controller.
  *
- * @param {int} roleID, 1 Autentihected, 2 Public, etc
+ * This utility is necessary in testing, because the test database is blank for each run
+ * and doesn't have any persmissions set, which means the test runner can't access them.
+ *
+ * @param {int} roleID, 1 Authenticated, 2 Public, etc
  * @param {string} value, in form or dot string eg `"permissions.users-permissions.controllers.auth.changepassword"`
  * @param {boolean} enabled, default true
  * @param {string} policy, default ''
  */
- const grantPriviledge = async (
-  roleID = 1,
-  value,
-  enabled = true,
-  policy = ""
-) => {
+const grantPriviledge = async (roleID, value, enabled = true, policy = "") => {
   const updateObj = value
     .split(".")
     .reduceRight((obj, next) => ({ [next]: obj }), { enabled, policy });
