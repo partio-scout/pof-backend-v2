2eeeddaf7adbd74c881202e04f53c0d5a438cda6
Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>
2021-07-08 15:22:35 +0300

    Added caching middleware

---
+++ config/middleware.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  settings: {
+    cache: {
+      enabled: true,
+      // This dictates which content types are cached
+      models: ["activities", "age-groups", "activity-groups", "suggestions", "comments"],
+    },
+  },
+});
