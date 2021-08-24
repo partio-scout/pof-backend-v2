3511ab7a4e30869cfbf3576f8ab74a41b0b3edf0
Tuomas Inkinen <tuomasink@gmail.com>
2021-08-24 12:02:01 +0300

    Change tracker (#27)
    * Added plugin for deploying the site from github
    
    * Added plugin 'change-tracker' for tracking changes to content
    
    * Remove workflow entries from content-changes
    
    * Updated README
    
    Co-authored-by: Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>

--- config/server.js
+++ config/server.js
@@ -1,10 +1,17 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '255a8bb6c62d421ce19160f15bea8aa9'),
+      secret: env("ADMIN_JWT_SECRET", "255a8bb6c62d421ce19160f15bea8aa9"),
     },
-    watchIgnoreFiles: ['**/migrations/**', '**/tests/**', '**/algolia-settings/**']
+    watchIgnoreFiles: [
+      "**/migrations/**",
+      "**/tests/**",
+      "**/algolia-settings/**",
+    ],
+  },
+  cron: {
+    enabled: true,
   },
 });
