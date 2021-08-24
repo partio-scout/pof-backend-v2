e3f8daede69b3ce8a35a0ad5bed8c672aef89f26
Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>
2021-08-24 12:38:36 +0300

    Add url to server configuration

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env("PUBLIC_URL", env("WEBSITE_HOSTNAME", "")),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "255a8bb6c62d421ce19160f15bea8aa9"),
