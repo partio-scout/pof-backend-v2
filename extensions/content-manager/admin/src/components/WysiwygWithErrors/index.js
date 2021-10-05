7cd653d4f312598bd63b5c3d79e86a79c5ec7c1d
Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>
2021-10-05 09:48:58 +0300

    Fixed TinyMce errors

--- extensions/content-manager/admin/src/components/WysiwygWithErrors/index.js
+++ extensions/content-manager/admin/src/components/WysiwygWithErrors/index.js
@@ -1,11 +1,10 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { isEmpty } from "lodash";
-import { Button } from "@buffetjs/core";
+import { Button, Error } from "@buffetjs/core";
+import { Description, ErrorMessage } from '@buffetjs/styles';
 import {
   Label,
-  InputDescription,
-  InputErrors,
   useUser,
 } from "strapi-helper-plugin";
 import Editor from "./Tinymce";
@@ -13,12 +12,14 @@ import MediaLib from "./MediaLib";
 
 const WysiwygWithErrors = ({
   inputDescription,
+  error: inputError,
   errors,
   label,
   name,
   noErrorsDescription,
   onChange,
   value,
+  validations,
 }) => {
   const user = useUser();
 
@@ -39,57 +40,99 @@ const WysiwygWithErrors = ({
     spacer = <div />;
   }
   const handleToggle = () => setIsOpen((prev) => !prev);
-  const handleChange = (data) => {
-    if (data.mime.includes("image") && typeof window !== "undefined") {
-      /**
-       * @tutorial Check the data object from media library, to insert the options you need to the image.
-       * @example console.log(data);
-       * @type {string}
-       */
-      const imgTag = `<p><img src="${data.url}" alt="${data.alternativeText}"/></p>`;
-      window.tinymce.activeEditor.insertContent(imgTag);
-      const newValue = window.tinymce.activeEditor.getContent();
 
-      onChange({ target: { name, value: newValue } });
-    }
-    // Handle videos and other type of files by adding some code
+  const handleImage = (data) => {
+    return `<p><img src="${data.url}" alt="${data.alternativeText}"/></p>`;
   };
 
+  const handleVideo = (data) => {
+    return `
+      <video width="320" height="240" controls data-url="${data.url}" data-mime="${data.mime}">
+        <source src="${data.url}" type="${data.mime}">
+        Your browser does not support the video tag.
+      </video>
+    `;
+  };
+
+  const handleFile = (data) => {
+    return `<a href="${data.url}">${data.url}</a>`;
+  };
+
+  const handleChange = (data) => {
+    if (typeof window === "undefined") return;
+
+    let change;
+    if (data.mime.includes("image")) {
+      change = handleImage(data);
+    } else if (data.mime.includes("video")) {
+      change = handleVideo(data);
+    } else if (data.mime.includes("application/pdf")) {
+      change = handleFile(data);
+    } else {
+      console.log("TinyMCE: Unkown data:", data);
+    }
+
+    if (change) {
+      window.tinymce.activeEditor.insertContent(change);
+      const newValue = window.tinymce.activeEditor.getContent();
+      onChange({ target: { name, value: newValue } });
+    }
+  };
+  console.log(noErrorsDescription, errors);
+  console.log(validations);
   return (
-    <div
-      className="col-12"
-      style={{
-        marginBottom: "1.6rem",
-        fontSize: "1.3rem",
-        fontFamily: "Lato",
-      }}
+    <Error
+      inputError={inputError}
+      name={name}
+      type="text"
+      validations={validations}
     >
-      <Label htmlFor={name} message={label} style={{ marginBottom: 10 }} />
-      <div>
-        <Button
-          style={{ lineHeight: "1", marginBottom: "16px" }}
-          color="primary"
-          onClick={handleToggle}
-        >
-          Media Library
-        </Button>
-      </div>
-      <Editor name={name} onChange={onChange} value={value || ""} isAdmin={isAdmin} />
-      <InputDescription
-        message={inputDescription}
-        style={!isEmpty(inputDescription) ? { marginTop: "1.4rem" } : {}}
-      />
-      <InputErrors
-        errors={(!noErrorsDescription && errors) || []}
-        name={name}
-      />
-      {spacer}
-      <MediaLib
-        onToggle={handleToggle}
-        isOpen={isOpen}
-        onChange={handleChange}
-      />
-    </div>
+      {({ canCheck, onBlur, error, dispatch }) => {
+        const hasError = Boolean(error);
+
+        return (
+          <div
+            className="col-12"
+            style={{
+              marginBottom: "1.6rem",
+              fontSize: "1.3rem",
+              fontFamily: "Lato",
+            }}
+          >
+            <Label
+              htmlFor={name}
+              message={label}
+              style={{ marginBottom: 10 }}
+            />
+            <div>
+              <Button
+                style={{ lineHeight: "1", marginBottom: "16px" }}
+                color="primary"
+                onClick={handleToggle}
+              >
+                Media Library
+              </Button>
+            </div>
+            <Editor
+              name={name}
+              onChange={onChange}
+              value={value || ""}
+              isAdmin={isAdmin}
+            />
+            {!hasError && inputDescription && (
+              <Description>{inputDescription}</Description>
+            )}
+            {hasError && <ErrorMessage>{error}</ErrorMessage>}
+            {spacer}
+            <MediaLib
+              onToggle={handleToggle}
+              isOpen={isOpen}
+              onChange={handleChange}
+            />
+          </div>
+        );
+      }}
+    </Error>
   );
 };
 
