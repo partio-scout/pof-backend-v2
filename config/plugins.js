107dd51533aa5a23a3734ae153847f6b874a76db
Linnea Myllynen <linnea.myllynen@orangit.fi>
2023-10-31 16:56:45 +0200

    enable plugins

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = ({ env }) => ({});
+module.exports = ({ env }) => ({
+  "deploy-site": {
+    enabled: true,
+    resolve: "./src/plugins/deploy-site",
+  },
+  guide: {
+    enabled: true,
+    resolve: "./src/plugins/guide",
+  },
+  "search-indexer": {
+    enabled: true,
+    resolve: "./src/plugins/search-indexer",
+  },
+});
