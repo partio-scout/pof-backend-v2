03f5f8d3e8c39d863842db6f23af30295e98513d
Linnea Myllynen <linnea.myllynen@orangit.fi>
2023-11-21 14:05:11 +0200

    use local postgres database instead of sqlite

--- config/database.js
+++ config/database.js
@@ -1,11 +1,16 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_V4_HOST", "localhost"),
+      port: env.int("DATABASE_V4_PORT", 5432),
+      database: env("DATABASE_V4_DATABASE", "strapi_v4"),
+      user: env("DATABASE_V4_USER", "user"),
+      password: env("DATABASE_V4_PASSWORD", "pass"),
+      schema: env("DATABASE_V4_SCHEMA", "public"), // Not required
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
