ca83e791341d7152ecaec20d1701ca7da04313d0
Linnea Myllynen <linnea.myllynen@orangit.fi>
2023-11-22 15:14:44 +0200

    use sendgrig email provider

--- config/plugins.js
+++ config/plugins.js
@@ -12,13 +12,15 @@ module.exports = ({ env }) => ({
     resolve: "./src/plugins/search-indexer",
   },
   email: {
-    provider: "sendgrid",
-    providerOptions: {
-      apiKey: env("SENDGRID_API_KEY"),
-    },
-    settings: {
-      defaultFrom: env("SENDGRID_DEFAULT_FROM_ADDRESS"),
-      defaultReplyTo: env("SENDGRID_DEFAULT_REPLY_TO_ADDRESS"),
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: env("SENDGRID_DEFAULT_FROM_ADDRESS"),
+        defaultReplyTo: env("SENDGRID_DEFAULT_REPLY_TO_ADDRESS"),
+      },
     },
   },
 });
